3d5c42c271cec388f8e8aed47a57e8663ee36945
Vadim Vidnichuk <VadimXPZL@gmail.com>
2017-11-07 16:28:41 +0300

    add java dock to vacancy skill.

--- main-ms/src/main/java/com/vidnichuk/ISOGJ/api/entity/VacancySkill.java
+++ main-ms/src/main/java/com/vidnichuk/ISOGJ/api/entity/VacancySkill.java
@@ -13,34 +13,70 @@ public class VacancySkill {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idVacancySkill;
 
+    /**
+     * vacancy object
+     */
     @ManyToOne
         @JoinColumn(name = "Vacancy")
     private Vacancy vacancy;
 
+    /**
+     * skill object
+     */
     @ManyToOne
         @JoinColumn(name = "Skill")
     private Skill skill;
 
+    /**
+     *
+     * @return
+     * id of vacancy skill
+     */
     public int getIdVacancySkill() {
         return idVacancySkill;
     }
 
+    /**
+     *
+     * @param idVacancySkill
+     * set id of vacancy skill
+     */
     public void setIdVacancySkill(int idVacancySkill) {
         this.idVacancySkill = idVacancySkill;
     }
 
+    /**
+     *
+     * @return
+     * vacancy object
+     */
     public Vacancy getVacancy() {
         return vacancy;
     }
 
+    /**
+     *
+     * @param vacancy
+     * set vacancy object
+     */
     public void setVacancy(Vacancy vacancy) {
         this.vacancy = vacancy;
     }
 
+    /**
+     *
+     * @return
+     * skill object
+     */
     public Skill getSkill() {
         return skill;
     }
 
+    /**
+     *
+     * @param skill
+     * set skill object
+     */
     public void setSkill(Skill skill) {
         this.skill = skill;
     }
