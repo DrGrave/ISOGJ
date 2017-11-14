64f6cb7da4dab4a54416cf575f9f4933e719844f
Aliaksandr Karpovich <akar@epolsoft.com>
2017-11-14 15:43:35 +0300

    added flyway config

---
+++ main-ms/src/main/resources/db/migration/V1_2__main_data.sql
@@ -0,0 +1,7 @@
+#
+# INSERT INTO isogjdb.type_of_skill (name_of_type_skill) VALUES ('DB');
+# INSERT INTO isogjdb.type_of_skill (name_of_type_skill) VALUES ('IT');
+# INSERT INTO isogjdb.skill (name_of_skill, type_of_skill) VALUES ('MySql', 1);
+# INSERT INTO isogjdb.type_of_user (name_of_type_user) VALUES ('Worker');
+# INSERT INTO isogjdb.user (active, email, user_name, user_password, user_patronymic, user_surname, gender, type_of_user, city_of_living) VALUES (1, 'vv@gmail.com', 'Petya', 'ffffff', 'Petya1', 'Patu1', null, null, null);
+# INSERT INTO isogjdb.user_skill (skill_mark, skill_practical_mark, id_skill, id_user, id_marker) VALUES ('4.5', '5.3', 1, 1, null);
