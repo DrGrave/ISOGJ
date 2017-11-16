848a7f02d4bf0b2a21d0301cc36a0f3bd0c90494
vadim <Vadim3111@tut.by>
2017-11-16 20:08:32 +0300

    Merge branch 'master' of https://github.com/DrGrave/ISOGJ
    # Conflicts:
    #	main-ms/src/main/java/com/vidnichuk/isogj/api/service/UserService.java
    #	main-ms/src/main/java/com/vidnichuk/isogj/impl/controller/LoginController.java

--- main-ms/src/main/java/com/vidnichuk/isogj/impl/controller/LoginController.java
+++ main-ms/src/main/java/com/vidnichuk/isogj/impl/controller/LoginController.java
@@ -6,9 +6,15 @@ import com.vidnichuk.isogj.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 
 
 /**
